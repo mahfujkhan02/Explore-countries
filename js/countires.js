94a10c8761a9de1def5e460fc5cfd3c6f7814178
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-02-07 12:55:07 +0600

    data display function added

--- js/countires.js
+++ js/countires.js
@@ -4,8 +4,16 @@ const loadCountries = () =>{
     fetch('https://restcountries.com/v3.1/all')
     
     .then(res => res.json())
-    .then(data => console.log(data))
-    
+    .then(data => displayCountries(data))
+
+   
     }
 
-    loadCountries()
+    const displayCountries = countries =>{
+
+        for(const country of countries)
+        console.log(country)
+
+}
+
+loadCountries()
