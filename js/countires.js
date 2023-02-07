30f187fc520b1043262e33b9a784375e0cb2f435
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-02-07 13:42:45 +0600

    countries container and json fucton added

--- js/countires.js
+++ js/countires.js
@@ -20,9 +20,34 @@ const loadCountries = () =>{
  
          const countryDiv = document.createElement('div');
          countryDiv.classList.add('country')
-         console.log(country)
-}
+         //console.log(country)
 
-       )};  
+         countryDiv.innerHTML =  `
+         <h3> Name : ${country.name.common} <h3>
+         <p>Capital : ${country.capital ? country.capital [0] : 'No capital'}</p>
+         <button onclick ="loadCountryDetail('${country.cca2}')">Details</button>
+         
+         `;
+        countryContainer.appendChild(countryDiv)
+
+        console.log(country)
+
+
+       });  
+
+    }
+
+    const loadCountryDetail = (code) => {
+        //https://restcountries.com/v3.1/alpha/{code}
+      
+        const url = `https://restcountries.com/v3.1/alpha/${code}`
+      
+        //console.log('get country details', code);
+        //console.log(url)
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>displayCountryDetails(data[0]))
+      
+      }
 
 loadCountries()
