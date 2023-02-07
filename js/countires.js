4db0bbb6127eeede3c268c21d2703eb56263c432
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-02-07 12:29:04 +0600

    data loaded

---
+++ js/countires.js
@@ -0,0 +1,11 @@
+// console.log("Tanim")
+
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+    }
+
+    loadCountries()
